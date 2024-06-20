ff22c6310071d09a308ee4083b2143e686a6c169
awandererNaiit <156669676+awandererNaiit@users.noreply.github.com>
2024-06-20 18:55:55 +0300

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    position VARCHAR(50),
+    hire_date DATE
+);
+
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    order_date DATE,
+    customer_id INT REFERENCES customers(customer_id),
+    total_amount DECIMAL(10, 2),
+    employee_id INT REFERENCES employees(employee_id)
+);
